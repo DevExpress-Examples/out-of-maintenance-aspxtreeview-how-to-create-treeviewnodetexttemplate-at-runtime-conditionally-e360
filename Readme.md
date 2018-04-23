d26f42451e6c31ba095fc3f1d5c6b1e4a5158d30
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# ASPxTreeView - How to create TreeViewNode.TextTemplate at runtime conditionally
+
+
+<p>This example illustrates how to create a template and assign it to TreeViewNode at runtime conditionally. As ASPxTreeView is bound to a datasource in this example, it is necessary to handle the ASPxTreeView.DataBound event to refer to a node.</p>
+
+<br/>
+
+
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,56 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports DevExpress.Web.ASPxTreeView
+Imports System.Xml
+Imports DevExpress.Web.ASPxEditors
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+		If (Not IsPostBack) Then
+			treeView.DataBind()
+			treeView.ExpandToDepth(0)
+		End If
+	End Sub
+
+	Protected Sub treeView_NodeDataBound(ByVal sender As Object, ByVal e As TreeViewNodeEventArgs)
+		Dim node As TreeViewNode = TryCast(e.Node, TreeViewNode)
+		Dim dataNode As XmlNode = (TryCast((TryCast(e.Node.DataItem, IHierarchyData)).Item, XmlNode))
+		If dataNode.Name = "class" Then
+			node.TextTemplate = New TextColorItemTemplate()
+		Else
+			node.TextTemplate = New TextItemTemplate()
+		End If
+	End Sub
+
+	Private Class TextItemTemplate
+		Implements ITemplate
+		Public Sub InstantiateIn(ByVal container As Control) Implements ITemplate.InstantiateIn
+			Dim nodeContainer As TreeViewNodeTemplateContainer = TryCast(container, TreeViewNodeTemplateContainer)
+			Dim lb As New ASPxLabel()
+			lb.ID = lb.ClientID
+			lb.Text = nodeContainer.Node.Text
+
+			container.Controls.Add(lb)
+		End Sub
+	End Class
+
+	Private Class TextColorItemTemplate
+		Implements ITemplate
+		Public Sub InstantiateIn(ByVal container As Control) Implements ITemplate.InstantiateIn
+			Dim nodeContainer As TreeViewNodeTemplateContainer = TryCast(container, TreeViewNodeTemplateContainer)
+			Dim lb As New ASPxLabel()
+			lb.ID = lb.ClientID
+
+			lb.Text = nodeContainer.Node.Text
+			lb.ForeColor = System.Drawing.Color.Blue
+
+			container.Controls.Add(lb)
+		End Sub
+	End Class
+End Class
